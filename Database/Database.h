c9febc8cd8ccf41ba014db00612be3c00b7556ca
Morfey <stalker22813372@mail.ru>
2025-05-31 17:15:49 +0300

    add interface DataBase

---
+++ Database/Database.h
@@ -0,0 +1,33 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include "../json.hpp"
+#include "../ComponentBase/ComponentBase.h"
+
+using json = nlohmann::ordered_json;
+
+class Database {
+    json db;
+    std::shared_ptr<ComponentBase> db_ptr;
+    static std::unique_ptr<Database> instance;
+
+    Database() = default;
+
+public:
+    Database(const Database&) = delete;
+    Database& operator=(const Database&) = delete;
+
+    static Database& get_instance();
+
+    void set_data(const std::shared_ptr<ComponentBase> new_data);
+
+    void save(const std::string& path);
+    void load(const std::string& path);
+
+    std::shared_ptr<ComponentBase> data();
+};
+
+#endif //DATABASE_H
