7bbb1cac86033852e92b11f6091d49d6afae90d3
Morfey <stalker22813372@mail.ru>
2025-06-01 00:57:04 +0300

    add MarkStatus

---
+++ MarkStatus.h
@@ -0,0 +1,14 @@
+#ifndef MARKSTATUS_H
+#define MARKSTATUS_H
+
+enum class MarkStatus {
+    None = -1,
+    Fail = 0,
+    Pass = 1,
+    NotSatisfactory = 2,
+    Satisfactory = 3,
+    Well = 4,
+    Great = 5
+};
+
+#endif //MARKSTATUS_H
