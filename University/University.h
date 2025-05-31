45f6c5111b67b0ba0c1199d3a616b9d660662384
Morfey <stalker22813372@mail.ru>
2025-05-31 17:27:54 +0300

    add interface University

---
+++ University/University.h
@@ -0,0 +1,16 @@
+#ifndef UNIVERSITY_H
+#define UNIVERSITY_H
+
+#include "../ComponentBase/ComponentBase.h"
+
+class University : public ComponentBase {
+public:
+    explicit University(const std::string& name);
+
+    void print(int indent) const override;
+
+    json to_json() const override;
+    static std::shared_ptr<University> from_json(const json& j);
+};
+
+#endif //UNIVERSITY_H
