e87aa9566ed07abfbdef2b4ed0efbba563c29e33
Morfey <stalker22813372@mail.ru>
2025-05-31 18:27:03 +0300

    add interface FivePointSystem

---
+++ AssessmentSystem/FivePointSystem/FivePointSystem.h
@@ -0,0 +1,13 @@
+#ifndef FIVEPOINTSYSTEM_H
+#define FIVEPOINTSYSTEM_H
+
+#include "../AssessmentSystem.h"
+
+class FivePointSystem : public AssessmentSystem {
+public:
+    FivePointSystem();
+
+    std::pair<bool, int> evaluate(const std::vector<int>& marks) const override;
+};
+
+#endif //FIVEPOINTSYSTEM_H
