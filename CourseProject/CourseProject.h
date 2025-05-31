9e7cf69bffa72bff909ad328238c6e01277dcc96
Morfey <stalker22813372@mail.ru>
2025-06-01 01:26:20 +0300

    add interface CourseProject

---
+++ CourseProject/CourseProject.h
@@ -0,0 +1,24 @@
+#ifndef COURSEPROJECT_H
+#define COURSEPROJECT_H
+
+#include "../AssessmentSystem/AssessmentSystem.h"
+#include "../ComponentBase/ComponentBase.h"
+#include "../MarkStatus.h"
+
+class CourseProject: public ComponentBase {
+    MarkStatus mark;
+    std::shared_ptr<AssessmentSystem> assessment_system;
+
+public:
+    CourseProject(const std::shared_ptr<AssessmentSystem> system, MarkStatus mark_ = MarkStatus::None);
+
+    void set_mark(int new_mark);
+    MarkStatus get_mark() const;
+
+    void print(int indent = 0) const override;
+
+    json to_json() const override;
+    static std::shared_ptr<CourseProject> from_json(const json& j);
+};
+
+#endif //COURSEPROJECT_H
