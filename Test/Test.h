9a8bd34cf51db21c7fde7d51f123c410f2638497
Morfey <stalker22813372@mail.ru>
2025-06-01 01:08:30 +0300

    add interface Test

---
+++ Test/Test.h
@@ -0,0 +1,27 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "../AssessmentSystem/AssessmentSystem.h"
+#include "../ComponentBase/ComponentBase.h"
+#include "../DateAndAssessSys/DateAndAssessSys.h"
+#include "../AssessmentSystem/PassFailSystem/PassFailSystem.h"
+#include "../MarkStatus.h"
+#include <string>
+#include <memory>
+
+class Test: public ComponentBase, public DateAndAssessSys {
+    MarkStatus mark;
+
+public:
+    Test(const std::shared_ptr<AssessmentSystem> system, MarkStatus mark_ = MarkStatus::None);
+
+    void set_mark(int new_mark);
+    MarkStatus get_mark() const;
+
+    void print(int indent = 0) const override;
+
+    json to_json() const override;
+    static std::shared_ptr<Test> from_json(const json& j);
+};
+
+#endif //TEST_H
